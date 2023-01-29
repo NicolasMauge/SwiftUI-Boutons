4ed4339e6fbf90befe84518d664132b34444c977
Nicolas MAUGE <maugenicolas@gmail.com>
2023-01-29 19:53:49 +0100

    Initial Commit

---
+++ testBoutons/testBoutonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testBoutonsApp.swift
+//  testBoutons
+//
+//  Created by Nicolas MAUGE on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct testBoutonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
